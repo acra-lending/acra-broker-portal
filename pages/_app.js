6955f813b4b6f7912c17d6d6a2f30f60069127ae
Addy Abid <Addy.Abid@CSC.local>
2022-10-11 17:00:34 -0700

    ip

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import '../styles/globals.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
+import { ProSidebarProvider } from 'react-pro-sidebar';
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ProSidebarProvider>
+      <Component {...pageProps} />
+    </ProSidebarProvider>
+    )
+    
 }
 
 export default MyApp
