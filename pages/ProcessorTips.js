5a45cf80cadcd56aefe98f8156d78b87d41840b8
Addy Abid <Addy.Abid@CSC.local>
2022-10-13 11:44:49 -0700

    added dummy data for tabs content

--- pages/ProcessorTips.js
+++ pages/ProcessorTips.js
@@ -6,6 +6,7 @@ import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import { useState } from 'react';
 function processorTips() {
+
     const [value, setValue] = useState('1');
 
     const handleChange = (event, newValue) => {
@@ -57,7 +58,7 @@ function processorTips() {
             </tr>
           </tbody>
         </table>
-        <p className='text-slate-500 dark:text-slate-400 text-base pl-8 pt-8'>
+        <p className='text-slate-500 dark:text-slate-400 text-base pl-8 pt-8 pb-8'>
                   Loss Payee Clause<br></br>
                   Citadel Servicing Corporation ISAOA<br></br>
                   25531 Commercentre Drive, Suite 160<br></br>
@@ -74,9 +75,110 @@ function processorTips() {
             <Tab label="CD/Closing" value="3" />
           </TabList>
         </Box>
-        <TabPanel value="1">File Flows</TabPanel>
-        <TabPanel value="2">Underwriting</TabPanel>
-        <TabPanel value="3">CD/Closing</TabPanel>
+        <TabPanel value="1">
+          <table className="border-collapse table-auto w-full text-sm">
+              <thead>
+              <tr>
+                  <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Filename</th>
+                  <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left"></th>
+                  <th className="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Download</th>
+              </tr>
+              </thead>
+              <tbody className="bg-white dark:bg-slate-800">
+              <tr>
+                  <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">Wet Dry States</td>
+                  <td className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400"></td>
+                  <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">Download</td>
+              </tr>
+              <tr>
+                  <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">Document Expiration Dates</td>
+                  <td className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400"></td>
+                  <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">Download</td>
+              </tr>
+              <tr>
+                  <td className="border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">TRID Calendar</td>
+                  <td className="border-b border-slate-200 dark:border-slate-600 p-4 text-slate-500 dark:text-slate-400"></td>
+                  <td className="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">Download</td>
+              </tr>
+              <tr>
+                  <td className="border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">Pre-Screen Process</td>
+                  <td className="border-b border-slate-200 dark:border-slate-600 p-4 text-slate-500 dark:text-slate-400"></td>
+                  <td className="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">Download</td>
+              </tr>
+              <tr>
+                  <td className="border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">4506-C Instructions</td>
+                  <td className="border-b border-slate-200 dark:border-slate-600 p-4 text-slate-500 dark:text-slate-400"></td>
+                  <td className="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">Download</td>
+              </tr>
+              </tbody>
+          </table>
+        </TabPanel>
+        <TabPanel value="2">
+        <table className="border-collapse table-auto w-full text-sm">
+            <thead>
+            <tr>
+                <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Filename</th>
+                <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left"></th>
+                <th className="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Download</th>
+            </tr>
+            </thead>
+            <tbody className="bg-white dark:bg-slate-800">
+            <tr>
+                <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">Cond 4503 Vetting Training</td>
+                <td className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400"></td>
+                <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">Download</td>
+            </tr>
+            <tr>
+                <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">Vetting Review</td>
+                <td className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400"></td>
+                <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">Download</td>
+            </tr>
+            <tr>
+                <td className="border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">Cond 4503 - Secure Insight Registration</td>
+                <td className="border-b border-slate-200 dark:border-slate-600 p-4 text-slate-500 dark:text-slate-400"></td>
+                <td className="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">Download</td>
+            </tr>
+            <tr>
+                <td className="border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">Secure Insights - Acra's Agents List</td>
+                <td className="border-b border-slate-200 dark:border-slate-600 p-4 text-slate-500 dark:text-slate-400"></td>
+                <td className="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">Download</td>
+            </tr>
+            <tr>
+                <td className="border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">Interest Only Calculator</td>
+                <td className="border-b border-slate-200 dark:border-slate-600 p-4 text-slate-500 dark:text-slate-400"></td>
+                <td className="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">Download</td>
+            </tr>
+            </tbody>
+        </table>
+        </TabPanel>
+        <TabPanel value="3">
+        <table className="border-collapse table-auto w-full text-sm">
+            <thead>
+            <tr>
+                <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Filename</th>
+                <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left"></th>
+                <th className="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Download</th>
+            </tr>
+            </thead>
+            <tbody className="bg-white dark:bg-slate-800">
+            <tr>
+                <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">Taxing Nuances</td>
+                <td className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400"></td>
+                <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">Download</td>
+            </tr>
+            <tr>
+                <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">Subject Property FAQ Feb 2019</td>
+                <td className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400"></td>
+                <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">Download</td>
+            </tr>
+            <tr>
+                <td className="border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">Seller Credit Apr 2019</td>
+                <td className="border-b border-slate-200 dark:border-slate-600 p-4 text-slate-500 dark:text-slate-400"></td>
+                <td className="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">Download</td>
+            </tr>
+            </tbody>
+        </table>
+        </TabPanel>
       </TabContext>
     </Box>
     </>
