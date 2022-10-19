f63b922a2eada1b25a01cf26e573e26b91a21c60
Addy Abid <Addy.Abid@CSC.local>
2022-10-19 16:56:23 -0700

    integrated strapi api for dashboard link items and passed them to dashlinks component

--- components/DashLinks.js
+++ components/DashLinks.js
@@ -1,4 +1,7 @@
-function DashLinks () {
+import SVG from 'react-inlinesvg';
+
+function DashLinks ({ props }) {
+    console.log(props.data.map((item, key) => item))
     const dashBoardItems = [
         'Cutoff Dates',
         'Turn Times',
@@ -12,8 +15,19 @@ function DashLinks () {
     ];
     return (
         <div className="max-h-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-3 pt-4">
-            {dashBoardItems.map((item, key) => (
-                <div key={key} className="flex justify-center items-center break-normal font-medium text-lg border-1 rounded-xl p-6 bg-white drop-shadow-xl">{item}</div>
+            {props?.data.map((item, key) => (
+                <div key={key} className="grid grid-cols-2 bg-white border-1 rounded-xl drop-shadow-lg justify-center ">
+                    <div className='flex justify-center items-center '>
+                        <SVG 
+                            src={item.attributes.icon}
+                            width={35}
+                        />
+                    </div>
+                    <div 
+                        className="flex justify-center items-center break-normal font-medium text-lg p-6">
+                            {item.attributes.title}
+                    </div>
+                </div>
             ))}
         </div>
     )
