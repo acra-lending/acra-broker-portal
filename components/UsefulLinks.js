ba0b167d7b910b72e8fa65a649fd740437bd18b8
Chris Liu <chrisl@CSC.local>
2022-10-26 08:01:35 -0700

    Fix spelling error

--- components/UsefulLinks.js
+++ components/UsefulLinks.js
@@ -2,7 +2,7 @@ function UsefulLinks ({ props }) {
     console.log(props)
     return (
         <div className="p-6">
-            <h5 className="text-gray-900 text-xl font-medium mb-2">Usefull Links</h5>
+            <h5 className="text-gray-900 text-xl font-medium mb-2">Useful Links</h5>
             <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
                 {props?.data.map(item => (
                     <a 
