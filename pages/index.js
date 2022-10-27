8152f5b051d928b1c918e428cd15047a5451e268
Addy Abid <Addy.Abid@CSC.local>
2022-10-26 17:03:04 -0700

    resolve merge conficts and add sellers guide structure

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,7 @@ import Navbar from '../components/NavBar'
 import DashLinks from "../components/DashLinks";
 import UsefulLinks from "../components/UsefulLinks";
 import Login from '../components/Login';
-// require('dotenv').config('../.env');
-import sellersGuide from "./sellers-guide";
+
 export default function Home({menuItems, dashboardItems, useLinksItems}) {
 
   const [isLogged, setIsLogged] = useState();
@@ -51,10 +50,26 @@ export default function Home({menuItems, dashboardItems, useLinksItems}) {
               </>
             )}
         </main>
-
-    </div>
+      </div>
   )
 }
+  {/* return (
+    <div className="relative w-full">
+      <Navbar />
+      <sellersGuide />
+    <div className="md:flex static">
+      <SideBar props={menuItems}/>
+
+      <div className="xl:mx-32 lg:mx-24 md:mx-auto md:pt-32">
+         <DashLinks props={dashboardItems}/>
+        <div>
+          <UsefulLinks props={useLinksItems}/>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+} */}
 export async function getServerSideProps(context) {
   const [menuResponse, dashResponse, useLinksResponse] = await Promise.all([
     fetch(`${process.env.BASE_URL}/broker-portal-menu-items`),
--- pages/sellers-guide.js
+++ pages/sellers-guide.js
@@ -1,13 +1,25 @@
-import { VueWrapper } from 'vuera';
-import Home from '../vue_component/Home.vue';
-import SearchResults from '../vue_component/SearchResults.vue';
-
+import { Accordion } from 'react-bootstrap';
+import  postData  from '../postData.json';
 function sellersGuide () {
+    console.log(postData);
     return (
         <div>
-            <VueWrapper
-                component={Home}
-            />
+         <Accordion>
+            {postData.Section?.map((Sections, index) => (
+                <Accordion.Item eventKey={index} key={index}>
+                    <Accordion.Header>{Sections.Title}</Accordion.Header>
+                    <Accordion.Body>
+                    {Sections?.Subsection?.map((subSection, index) => (
+                        <Accordion key={index}>
+                            <Accordion.Item eventKey={index}>
+                                <Accordion.Header>{subSection.Title}</Accordion.Header>
+                            </Accordion.Item>
+                        </Accordion>
+                    ))}
+                    </Accordion.Body>
+                </Accordion.Item>
+            ))}
+            </Accordion>
         </div>
     )
 }
