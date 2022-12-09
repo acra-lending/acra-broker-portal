7cb9811c993474addde6f42efa305e0250b9bac9
Addy Abid <Addy.Abid@CSC.local>
2022-12-08 16:41:42 -0800

    update title

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,17 @@ import '../styles/globals.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from 'next/head';
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
   return (
-    
+      <>
+      <Head>
+        <title>Acra Lending | Broker Portal</title>
+      </Head>
       <Component {...pageProps} />    
+      </>
     
     )
     
