8dc1c09a87845475d63049d34c34910a614e3856
Chris Liu <chrisl@CSC.local>
2022-12-09 10:23:50 -0800

    Change table text color

--- pages/forms.js
+++ pages/forms.js
@@ -68,9 +68,9 @@ function FormsTable({menuItems, formsItems}) {
                             <table className="border-collapse table-auto w-full text-sm">
                                 <thead>
                                 <tr>
-                                    <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Filename</th>
+                                    <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-600 dark:text-slate-500 text-left">Filename</th>
                                     {/* <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left"></th> */}
-                                    <th className="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Download</th>
+                                    <th className="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-600 dark:text-slate-500 text-left">Download</th>
                                 </tr>
                                 </thead>
                                 <tbody className="bg-white dark:bg-slate-800">
